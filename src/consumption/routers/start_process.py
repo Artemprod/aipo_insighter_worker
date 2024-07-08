c78c0cb467c80e8c2d4d823967336ff5ba9875fc
artemprod <artemprod1@gmail.com>
2024-07-08 14:03:06 +0400

    rabit ack nack

--- src/consumption/routers/start_process.py
+++ src/consumption/routers/start_process.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from faststream.rabbit import RabbitRouter, RabbitQueue
+from faststream.rabbit import RabbitRouter, RabbitQueue, RabbitMessage
 from faststream import Context
 
 from container import components
@@ -10,17 +10,29 @@ from src.consumption.queues.youtube import on_message_from_youtube_queue
 process_router = RabbitRouter(prefix="")
 
 
+async def handle_task_result(task, msg):
+    try:
+        exception = task.exception()
+        if exception:
+            await msg.nack(
+                requeue=True)  # Сообщаем, что сообщение не было обработано и нужно повторно добавить в очередь
+        else:
+            await msg.ack()  # Подтверждаем успешную обработку сообщения
+    except Exception as e:
+        await msg.nack(requeue=True)
+
+
 @process_router.subscriber(queue=RabbitQueue(name=components.rabit_consumers['youtube_consumer']['queue'],
                                              routing_key=components.rabit_consumers['youtube_consumer']['routing_key']),
                            exchange=components.rabit_consumers['youtube_consumer']['exchanger']['name'])
-async def handle_youtube_response(msg, context=Context()):
+async def handle_youtube_response(msg: RabbitMessage, context=Context()):
     task = asyncio.create_task(on_message_from_youtube_queue(message=msg, utils=context.utils))
-    task.add_done_callback(lambda t: t.exception() if t.exception() else None)
+    task.add_done_callback(lambda t: asyncio.create_task(handle_task_result(t, msg)))
 
 
 @process_router.subscriber(queue=RabbitQueue(name=components.rabit_consumers['storage_consumer']['queue'],
                                              routing_key=components.rabit_consumers['storage_consumer']['routing_key']),
                            exchange=components.rabit_consumers['storage_consumer']['exchanger']['name'])
-async def handle_s3_response(msg, context=Context()):
+async def handle_s3_response(msg: RabbitMessage, context=Context()):
     task = asyncio.create_task(on_message_from_s3(message=msg, utils=context.utils))
-    task.add_done_callback(lambda t: t.exception() if t.exception() else None)
+    task.add_done_callback(lambda t: asyncio.create_task(handle_task_result(t, msg)))
