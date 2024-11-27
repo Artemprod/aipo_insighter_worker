61d5bd854249835ca51338a053013ca698b42128
anatoly <anatolypolyakovd@gmail.com>
2024-11-27 21:16:18 +1000

    refactor start_process.py

--- src/consumption/routers/start_process.py
+++ src/consumption/routers/start_process.py
@@ -1,40 +1,47 @@
 import asyncio
 import json
+from typing import Type
+
 from faststream.rabbit import RabbitRouter, RabbitQueue, RabbitMessage
 from faststream import Context
 
 from container import components
+from src.consumption.queues.base_processor import BaseProcessor
 from src.consumption.queues.s3 import S3Processor
 from src.consumption.queues.youtube import YouTubeProcessor
 
 process_router = RabbitRouter(prefix="")
 
 
-@process_router.subscriber(queue=RabbitQueue(name=components.rabit_consumers['youtube_consumer']['queue'],
-                                             routing_key=components.rabit_consumers['youtube_consumer']['routing_key']),
-                           exchange=components.rabit_consumers['youtube_consumer']['exchanger']['name'])
+async def handle_message(msg: RabbitMessage, processor: Type[BaseProcessor], context: Context):
+    task = asyncio.create_task(
+        processor.handle_message(
+            message=json.loads(msg.body.decode("utf-8")),
+            utils=context.utils
+        )
+    )
+    task.add_done_callback(lambda t: t.exception() if t.exception() else None)
+    await asyncio.sleep(5)
+    await msg.ack()
+
+
+@process_router.subscriber(
+    queue=RabbitQueue(
+        name=components.rabit_consumers['youtube_consumer']['queue'],
+        routing_key=components.rabit_consumers['youtube_consumer']['routing_key']
+    ),
+    exchange=components.rabit_consumers['youtube_consumer']['exchanger']['name']
+)
 async def handle_youtube_response(msg: RabbitMessage, context=Context()):
-    task = asyncio.create_task(
-        YouTubeProcessor.handle_message(
-            message=json.loads(msg.body.decode("utf-8")),
-            utils=context.utils
-        )
-    )
-    task.add_done_callback(lambda t: t.exception() if t.exception() else None)
-    await asyncio.sleep(5)
-    await msg.ack()
+    await handle_message(msg, YouTubeProcessor, context)
 
 
-@process_router.subscriber(queue=RabbitQueue(name=components.rabit_consumers['storage_consumer']['queue'],
-                                             routing_key=components.rabit_consumers['storage_consumer']['routing_key']),
-                           exchange=components.rabit_consumers['storage_consumer']['exchanger']['name'])
+@process_router.subscriber(
+    queue=RabbitQueue(
+        name=components.rabit_consumers['storage_consumer']['queue'],
+        routing_key=components.rabit_consumers['storage_consumer']['routing_key']
+    ),
+    exchange=components.rabit_consumers['storage_consumer']['exchanger']['name']
+)
 async def handle_s3_response(msg: RabbitMessage, context=Context()):
-    task = asyncio.create_task(
-        S3Processor.handle_message(
-            message=json.loads(msg.body.decode("utf-8")),
-            utils=context.utils
-        )
-    )
-    task.add_done_callback(lambda t: t.exception() if t.exception() else None)
-    await asyncio.sleep(5)
-    await msg.ack()
+    await handle_message(msg, S3Processor, context)
