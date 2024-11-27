5a9b945e3289d3dc888851bdedd9d73b0b81ea4c
anatoly <anatolypolyakovd@gmail.com>
2024-11-27 21:31:50 +1000

    delete Field() in schemas.py

--- src/api/routers/main_process/schemas.py
+++ src/api/routers/main_process/schemas.py
@@ -3,34 +3,38 @@ from pydantic import BaseModel, Field
 
 
 class BaseMessage(BaseModel):
-    unique_id: str = Field()
-    user_id: int = Field()
-    assistant_id: int = Field()
-    publisher_queue: str = Field()
+    unique_id: str
+    user_id: int
+    assistant_id: int
+    publisher_queue: str
     source: Optional[str] = None
     user_prompt: Optional[str] = None
     description: Optional[str] = None
 
 
 class StartFromYouTubeMessageScheme(BaseMessage):
-    youtube_url: str = Field()
+    youtube_url: str
 
 
 class StartFromS3Scheme(BaseMessage):
-    s3_path: str = Field()
+    s3_path: str
     storage_url: Optional[str] = None
 
 
+
+class StartFromYouTubeMessageScheme(BaseMessage):
+    youtube_url: str
+
 # Не используются. Нужно ли их оставлять?
 class BaseErrorResponse(BaseModel):
-    user_id: int = Field()
+    user_id: int
     description: Optional[str] = None
 
 
 class StartFromYouTubeErrorResponseScheme(BaseErrorResponse):
-    youtube_url: str = Field()
+    youtube_url: str
 
 
 class StartFromStorageErrorResponseScheme(BaseErrorResponse):
-    file_path: str = Field()
+    file_path: str
     storage_url: Optional[str] = None
